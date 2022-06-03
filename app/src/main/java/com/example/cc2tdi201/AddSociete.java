a5d42dc0381b98b678fe298987975643e4803d6e
khayriAmina <aminakhayri2001@gmail.com>
2022-06-03 15:52:39 +0100

    q7

---
+++ app/src/main/java/com/example/cc2tdi201/AddSociete.java
@@ -0,0 +1,14 @@
+package com.example.cc2tdi201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSociete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_societe);
+    }
+}
--- app/src/main/java/com/example/cc2tdi201/MainActivity.java
+++ app/src/main/java/com/example/cc2tdi201/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cc2tdi201;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newForm(View view) {
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.btnAdd: i=new Intent(MainActivity.this, AddSociete.class); break;
+            case R.id.btnEdit: i=new Intent(MainActivity.this, EditSociete.class); break;
+            case R.id.btnLst: i=new Intent(MainActivity.this, listeSociete.class); break;
+
+        }
+        startActivity(i);
+
     }
 }
