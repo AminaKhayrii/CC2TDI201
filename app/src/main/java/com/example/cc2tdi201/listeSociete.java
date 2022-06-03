cc256d047a6ca08e8554c03d4c4d23d26cb03c2d
khayriAmina <aminakhayri2001@gmail.com>
2022-06-03 17:12:06 +0100

    q12

--- app/src/main/java/com/example/cc2tdi201/listeSociete.java
+++ app/src/main/java/com/example/cc2tdi201/listeSociete.java
@@ -3,6 +3,11 @@ package com.example.cc2tdi201;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class listeSociete extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class listeSociete extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_societe);
+
+        ListView liste;
+        MyDatabase data;
+
+        liste=findViewById(R.id.lst);
+        data=new MyDatabase(this);
+        ArrayList<Societe> e =MyDatabase.getAllSociete(data.getReadableDatabase());
+        ArrayList<HashMap<String,Object>> h = new ArrayList<>();
+        for(Societe sc : e){
+            HashMap<String,Object> hm =new HashMap<>();
+            hm.put("nom",sc.getNom());
+            hm.put("nb_employe",sc.getNbEmploye());
+            h.add(hm);
+        }
+        String[] from = {"nom","nb_employe"};
+        int [] tr ={R.id.txtNom,R.id.txtNB};
+        SimpleAdapter b = new SimpleAdapter(this,h,R.layout.activity_liste_societe,from,tr);
+        liste.setAdapter(b);
+
+
     }
 }
