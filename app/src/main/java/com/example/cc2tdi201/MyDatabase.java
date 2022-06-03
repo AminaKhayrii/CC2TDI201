6bbf57c3eb531c0220903b27d667912db5387d0c
khayriAmina <aminakhayri2001@gmail.com>
2022-06-03 15:23:05 +0100

    q5 a-b

--- app/src/main/java/com/example/cc2tdi201/MyDatabase.java
+++ app/src/main/java/com/example/cc2tdi201/MyDatabase.java
@@ -1,5 +1,6 @@
 package com.example.cc2tdi201;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,8 +10,8 @@ public class MyDatabase extends SQLiteOpenHelper {
     public static String table="Societe";
     public static String col1="id";
     public static String col2="nom";
-    public static String col3=", secteur_activite";
-    public static String col4=", nb_employe";
+    public static String col3="secteur";
+    public static String col4="nb_employe";
 
     public MyDatabase(Context c){
         super(c,Db_Name,null,1);
@@ -28,6 +29,24 @@ public class MyDatabase extends SQLiteOpenHelper {
         String req= "DROP TABLE " + table;
         sqLiteDatabase.execSQL(req);
         onCreate(sqLiteDatabase);
+
+    }
+
+    public static long AddSociete(SQLiteDatabase sqLiteDatabase, Societe e){
+        ContentValues ct=new ContentValues();
+        ct.put(col2,e.getNom());
+        ct.put(col3,e.getSecteur());
+        ct.put(col4,e.getNbEmploye());
+        return sqLiteDatabase.insert(table,null,ct);
+
+    }
+    public static long UpdateSociete(SQLiteDatabase sqLiteDatabase, Societe e){
+        ContentValues ct=new ContentValues();
+        ct.put(col2,e.getNom());
+        ct.put(col3,e.getSecteur());
+        ct.put(col4,e.getNbEmploye());
+        return sqLiteDatabase.update(table,ct,"id="+e.getId(),null);
+
 
     }
 }
