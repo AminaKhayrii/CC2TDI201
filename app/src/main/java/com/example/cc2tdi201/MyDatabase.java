dc39f92355b2e700a899c8ad0c0f70ee765a4f28
khayriAmina <aminakhayri2001@gmail.com>
2022-06-03 15:11:34 +0100

    q4

--- app/src/main/java/com/example/cc2tdi201/MyDatabase.java
+++ app/src/main/java/com/example/cc2tdi201/MyDatabase.java
@@ -4,6 +4,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDatabase extends SQLiteOpenHelper {
+    public static String Db_Name="societes.db";
+    public static String table="Societe";
+    public static String col1="id";
+    public static String col2="Raison_Sociale";
+    public static String col3=", Secteur_activite";
+    public static String col4=", nb_employe";
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
